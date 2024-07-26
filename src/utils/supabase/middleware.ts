b838bb5ca73abd4f8a5a422ead9e0ec2da9900fa
Alejandro Lopez <alejandrolpz0619@gmail.com>
2024-07-26 19:33:11 -0400

    chore: Update session middleware to exclude root path from redirection policy

--- src/utils/supabase/middleware.ts
+++ src/utils/supabase/middleware.ts
@@ -39,7 +39,7 @@ export async function updateSession(request: NextRequest) {
     !user &&
     !request.nextUrl.pathname.startsWith('/login') &&
     !request.nextUrl.pathname.startsWith('/auth') &&
-    !request.nextUrl.pathname.startsWith('/home') &&
+    !(request.nextUrl.pathname === '/') &&
     !request.nextUrl.pathname.startsWith('/about')
   ) {
     // no user, potentially respond by redirecting the user to the login page
