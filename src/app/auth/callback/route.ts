ea7ef1e6fd446b47135e70b20696f792dbc77c2a
Alejandro Lopez <alejandrolpz0619@gmail.com>
2024-07-31 19:24:11 -0400

    chore: Refactor auth callback route to handle load balancer and local environment

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -4,16 +4,24 @@ import { createClient } from '@/utils/supabase/server'
 
 export async function GET(request: Request) {
   const { searchParams, origin } = new URL(request.url)
-  console.log('searchParams', searchParams, request.url)
   const code = searchParams.get('code')
   // if "next" is in param, use it as the redirect URL
-  const next = searchParams.get('next') ?? '/home'
-  console.log('code', code, 'next', next)
+  const next = searchParams.get('next') ?? '/'
+
   if (code) {
     const supabase = await createClient()
     const { error } = await supabase.auth.exchangeCodeForSession(code)
     if (!error) {
-      return NextResponse.redirect(`${origin}${next}`)
+      const forwardedHost = request.headers.get('x-forwarded-host') // original origin before load balancer
+      const isLocalEnv = process.env.NODE_ENV === 'development'
+      if (isLocalEnv) {
+        // we can be sure that there is no load balancer in between, so no need to watch for X-Forwarded-Host
+        return NextResponse.redirect(`${origin}${next}`)
+      } else if (forwardedHost) {
+        return NextResponse.redirect(`https://${forwardedHost}${next}`)
+      } else {
+        return NextResponse.redirect(`${origin}${next}`)
+      }
     }
   }
 
