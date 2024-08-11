3104841617457451faccab8b9eedaff3eb6c93a8
Alejandro Lopez <alejandrolpz0619@gmail.com>
2024-08-11 14:32:58 -0400

    feat: Update HomeLayout with Chakra UI Menu and Logout functionality

--- src/app/home/layout.tsx
+++ src/app/home/layout.tsx
@@ -2,9 +2,17 @@
 import { createClient } from "@/utils/supabase/client";
 import Avatar from "@/components/Avatar";
 import { User } from "@supabase/supabase-js";
-import { redirect } from "next/navigation";
+import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 import Link from "next/link";
+import {
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuDivider,
+  MenuList,
+  Button,
+} from "@chakra-ui/react";
 
 export default function HomeLayout({
   children,
@@ -13,11 +21,12 @@ export default function HomeLayout({
 }) {
   const [user, setUser] = useState<User | null>(null);
   const supabase = createClient();
+  const router = useRouter();
   useEffect(() => {
     const fetchUser = async () => {
       const { data, error } = await supabase.auth.getUser();
       if (error || !data?.user) {
-        redirect("/auth");
+        router.replace("/auth");
       } else {
         setUser(data.user);
       }
@@ -25,6 +34,14 @@ export default function HomeLayout({
 
     fetchUser();
   }, [supabase]);
+
+  const handleLogout = async () => {
+    const { error } = await supabase.auth.signOut();
+    if (!error) {
+      router.replace("/auth");
+    }
+  };
+
   return (
     <div className="flex flex-col">
       <nav className="w-full flex justify-between items-center bg-custom-light text-custom-lighter shadow-md px-4 py-2 sm:px-6 md:px-8 lg:px-6 lg:py-2">
@@ -35,11 +52,24 @@ export default function HomeLayout({
             </span>
           </Link>
         </div>
-        <Avatar
-          placeholder={user?.email || user?.user_metadata.name}
-          size={50}
-          style="character"
-        />
+        <Menu>
+          <MenuButton as={Button} variant="link">
+            <div className=" my-2 flex items-center">
+              <Avatar
+                placeholder={user?.email || user?.user_metadata.name}
+                size={50}
+                style="character"
+              />
+            </div>
+          </MenuButton>
+          <MenuList>
+            <MenuItem>
+              <Link href="/profile">Perfil</Link>
+            </MenuItem>
+            <MenuDivider />
+            <MenuItem onClick={handleLogout}>Cerrar sesión</MenuItem>
+          </MenuList>
+        </Menu>
       </nav>
 
       {children}
